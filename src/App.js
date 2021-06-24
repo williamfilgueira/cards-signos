8556204efdc0d220e9b7f02f40d76be1aae4b481
William Filgueira <williamfilgueira@gmail.com>
2021-06-24 12:20:13 -0300

    implementado props nos atributos para poder reaproveitar o cógido

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListaSigno from './components/ListaSignos'
+
+function App() {
+  return (
+    <div className="App">
+     
+     <ListaSigno/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Titulo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './stylle.css';
+
+export default function Titulo(){
+
+    return(
+        <div className="box">
+            <p className="texto">Baralho de signos</p>
+        </div>
+    );
+}
